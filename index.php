bf9908f7422a5ca9bf517a0082325956966ab01a
kikosvg <karim1809252@miuegypt.edu.eg>
2021-05-30 20:57:23 +0200

    did somethings in home page

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <html>
 <head>
 <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<style>
+body {
+    background-image: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('background.jpg');
+}
+.btn {
+    outline: none !important;
+    box-shadow: none !important;
+    color:white !important;
+
+}
+.btn:hover, .btn:hover {
+    outline: none !important;
+    box-shadow: none !important;
+}    
+</style>
 </head>
 <body>
-<button type="button" class="btn btn-primary">Primary</button>
+<div class="container text-center">
+<p style="margin-top:10%;font-size: 110px; color:white;">Welcome to this coffee shop</p>
+<button style="width:350px; height: 130px;font-size: 30px;border-radius:25px;" type="button" class="btn btn-dark">Open menu</button>
 
+</div>
 </body>
 </html>
